6958bdc680f6bf7aa2b5fa7affd730358d32dd1c
oscaryangb <oscaryangb@gmail.com>
2026-07-21 18:46:37 -0400

    Tweak parameters

--- parameters.h
+++ parameters.h
@@ -9,11 +9,11 @@ constexpr size_t MAX_SIMULATION_LENGTH = SAMPLE_RATE * 300;
 
 constexpr size_t WAVEFORM_LENGTH = 2205;
 constexpr float WAVEFORM_PARTIAL_AMPLITUDE_VARIANCE = 0.2f;
-constexpr float WAVEFORM_PARTIAL_FALLOFF = 0.6f;
+constexpr float WAVEFORM_PARTIAL_FALLOFF = 0.55f;
 constexpr float WAVEFORM_PARTIAL_FALLOFF_VARIANCE = 0.1f;
 constexpr size_t WAVEFORM_HARMONICS = 10;
 
-constexpr float GLOBAL_LOWEST_FREQUENCY = 200.f;
+constexpr float GLOBAL_LOWEST_FREQUENCY = 150.f;
 constexpr float GLOBAL_HIGHEST_FREQUENCY = 800.f;
 constexpr float SHORTEST_BREATH_LENGTH = 3.f;
 constexpr float LONGEST_BREATH_LENGTH = 11.f;
@@ -24,8 +24,8 @@ constexpr float PITCH_VARIANCE = 0.01f;
 constexpr size_t PITCH_CANDIDATES = 5;
 constexpr size_t LONG_RMS_LENGTH = SAMPLE_RATE * 30;
 constexpr size_t SHORT_RMS_LENGTH = 4000;
-constexpr float STOPPING_THRESHOLD = 0.6f;
+constexpr float STOPPING_THRESHOLD = 0.55f;
 
 constexpr float MIN_VIBRATO_FREQUENCY = 4.f;
 constexpr float MAX_VIBRATO_FREQUENCY = 7.f;
-constexpr float MAX_VIBRATO_AMPLITUDE = 0.01f;
+constexpr float MAX_VIBRATO_AMPLITUDE = 0.015f;
